425d9a1178f013a0710e25dbf5b1415b226df2e9
Elcess <kelcess@alum.mit.edu>
2018-12-05 13:37:53 -0500

    Feature: Get All Campuses and Students

--- app/components/root.js
+++ app/components/root.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import AllCampuses from './AllCampuses';
 import AllStudents from './AllStudents';
 
 const Root = () => {
   return (
-    <div>
-      <nav>
-        Welcome!
-      </nav>
-      <main>
-        <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-        <BrowserRouter >
+    <BrowserRouter >
+      <div>
+        <nav>
+          <Link to='/'>
+            Welcome!
+        </Link>
+          <Link to='/campuses'>
+            Campuses
+        </Link>
+          <Link to='/students'>
+            Students
+        </Link>
+        </nav>
+        <main>
+          <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
           <Switch >
             <Route path='/campuses' component={AllCampuses} />
             <Route path='/students' component={AllStudents} />
           </Switch>
-        </BrowserRouter>
-      </main>
-    </div>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
